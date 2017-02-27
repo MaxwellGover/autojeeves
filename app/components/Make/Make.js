a7e90e6bf5ac30fdc57e0c17765b4791a03b25de
Maxwell Gover <maxwellrgover@gmail.com>
2017-02-26 19:50:02 -0500

    Fixed issue with ListItem components not displaying & created action creator to store users vehicle selection.

--- app/components/Make/Make.js
+++ app/components/Make/Make.js
@@ -3,7 +3,23 @@ import { View, Text, ListView, StyleSheet } from 'react-native'
 import { connect } from 'react-redux'
 import { List, ListItem } from 'react-native-elements'
 import NavigationBar from 'react-native-navbar'
-import { getMakeData } from '~/redux/modules/data'
+import { getMakeData, storeUserCar } from '~/redux/modules/data'
+
+// Dummy data
+const list = [
+  {
+  	id: 1,
+    name: 'Audi',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    subtitle: 'Vice President'
+  },
+  {
+  	id: 2,
+    name: 'Buick',
+    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
+    subtitle: 'Vice Chairman'
+  }
+];
 
 class Make extends Component {
 	static propTypes = {
@@ -14,14 +30,21 @@ class Make extends Component {
 		super(props)
 		this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2 })
 		this.state = {
-			dataSource: this.ds.cloneWithRows(this.props.makeData)
+			dataSource: this.ds.cloneWithRows(list)
 		}
 	}
 	componentDidMount () {
 		// this.props.dispatch(getMakeData())
 	}
-	renderRow = (item) => { 
-		return <ListItem key={item.id} title={item.name} /> 
+	renderRow = (car) => { 
+		return (
+			<ListItem 
+				key={car.id} 
+				title={car.name} 
+				onPress={() => this.props.dispatch(storeUserCar(car))}
+				underlayColor='#eceeef' 
+			/>
+		) 
 	}
 	render () {
 		console.log(this.props.makeData)
@@ -37,10 +60,10 @@ class Make extends Component {
 						Select the make of your vehicle
 					</Text>
 				</View>
-				<List style={{flex: 1}}>
+				<List containerStyle={{flex: 1, marginTop: 0}}>
 					<ListView 
-					renderRow={item => this.renderRow(item)}
-					dataSource={this.state.dataSource}
+						renderRow={car => this.renderRow(car)}
+						dataSource={this.state.dataSource}
 					/>
 				</List>
 			</View>
